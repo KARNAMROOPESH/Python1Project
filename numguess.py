bfee8762e4c7e7e9df8a6505cdeea611a0225d31
Karnam Roopesh <63301556+KARNAMROOPESH@users.noreply.github.com>
2021-04-22 18:42:53 +0530

    Add files via upload

--- numguess.py
+++ numguess.py
@@ -1,10 +1,17 @@
 import random
-usernumber = int(input("Please guess a number from 1 to 9 = "))
-print (usernumber)
-numguessed = int(random.randrange(1,9,1))
-if  numguessed == usernumber:
-        print('The number you guessed is PERFECT')
-if  numguessed > usernumber:
-        print('The number you guessed was LOW')
-if  numguessed < usernumber:
-        print('The number you guessed is HIGH')
+
+game = 0
+
+while (game < 5): 
+        usernumber = int(input("Please guess a number from 1 to 9 = "))
+        print (usernumber)
+        numguessed = int(random.randrange(1,9,1))
+        if  numguessed == usernumber:
+                print('The number you guessed is PERFECT')
+                game = 5
+        elif  numguessed > usernumber:
+                print('The number you guessed was LOW')
+                game = game+1
+        elif  numguessed < usernumber:
+                print('The number you guessed is HIGH')
+                game = game+1
